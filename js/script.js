44ccd7cded7c4811bdafa7ae09689a7857995851
marcotristani <marco.tristani@hotmail.it>
2025-12-10 14:01:32 +0100

    flat: cambiata immagine al primo click del bottone

--- js/script.js
+++ js/script.js
@@ -7,6 +7,12 @@ const btn = document.querySelector('button');
 console.log(lamp, btn);//Verifico selezioni
 
 //quando viene fatto un click sul bottone
-//allora l'immagine cambierà con l'immagine della lampadina accesa
+btn.addEventListener("click",
+    function () {
+        lamp.src = "./img/yellow_lamp.png";         //allora l'immagine cambierà con l'immagine della lampadina accesa
+        
+    }
+)
+
 // se l'immagine presente è quella della lampadina accesa allora quando viene fatto un click sul bottone
 //l'immagine cambierà con quella della lampadina spenta
