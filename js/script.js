5998edc2dac0459353bde6b9f63483e733bd8bc6
marcotristani <marco.tristani@hotmail.it>
2025-12-10 13:57:54 +0100

    flat: preparati richiami html e logica dell'esercizio in js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//Preparazione
+
+//selezioniamo i tag html che ci interessano
+const lamp = document.querySelector('img');
+const btn = document.querySelector('button');
+
+console.log(lamp, btn);//Verifico selezioni
+
+//quando viene fatto un click sul bottone
+//allora l'immagine cambierà con l'immagine della lampadina accesa
+// se l'immagine presente è quella della lampadina accesa allora quando viene fatto un click sul bottone
+//l'immagine cambierà con quella della lampadina spenta
